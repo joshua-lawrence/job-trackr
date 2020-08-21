52929778d15a624f8e642983c5782a334e0b13c7
Joshua Lawrence <joshua98lawrence@gmail.com>
2020-08-21 03:52:29 -0600

    Added a lot

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
 import React from 'react';
 import logo from './logo.svg';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Navbar } from 'react-bootstrap';
 import DataTable from './components/DataTable';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import AddApplication from './components/AddApplication';
 
 function App() {
   return (
-    <Container fluid style={{background: "#f8f8f8"}}> 
-      <Row>
-        <Col>
-        </Col>
-        <Col style={{background: "white"}}>
-          <DataTable />
-        </Col>
-      </Row>
+    <Container fluid style={{background: "#f8f8f8", paddingRight: '0', paddingLeft: '0'}}> 
+      <Navbar bg="dark" variant="dark">
+      <Navbar.Brand href="#home">
+        <img
+          alt=""
+          src="/logo.svg"
+          width="30"
+          height="30"
+          className="d-inline-block align-top"
+        />{' '}
+        Job Trackr
+      </Navbar.Brand>
+    </Navbar>
+      <Container>
+        <Row>
+            <Col>
+              <AddApplication />
+              <DataTable />
+            </Col>
+          </Row>
+      </Container>
     </Container>
   );
 }
--- src/components/DataTable.js
+++ src/components/DataTable.js
@@ -1,11 +1,13 @@
 import axios from 'axios';
 import React from 'react';
-import { Table, Spinner } from 'react-bootstrap';
+import { Table, Spinner, Alert, Button } from 'react-bootstrap';
 import { useEffect, useState } from 'react';
+import moment from 'moment';
 
 
 const DataTable = () => {
     const [applications, setApplications] = useState([]);
+    const [selectedIds, setSelectedIds] = useState([]);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -15,14 +17,34 @@ const DataTable = () => {
         fetchData();
     }, [])
 
-    const deleteLine = (id) => {
-        if(window.confirm("Are you sure you wish to delete this job application?")) {
-            axios.delete(`http://localhost:9090/delete/${id}`)
-            .then(res => {
-                console.log(res);
-                setApplications(applications.filter(item => item.id !== id));
-            })
+
+    const addChecked = (id) => {
+        if (!selectedIds.includes(id)) {
+            setSelectedIds(selectedIds.concat(id));
         }
+        else {
+            setSelectedIds(selectedIds.filter(selectedIds => selectedIds !== id));
+        }
+    }
+
+    const selectAll = () => {
+
+    }
+
+    const editBox = () => {
+        return (
+            <div style={{border: "1px solid #ddd"}}>
+                <Button variant="danger" onClick={() => deleteLine(selectedIds)}>Delete</Button>
+            </div>
+        )
+    }
+
+    const deleteLine = (id) => {
+        axios.delete(`http://localhost:9090/deleteapplication/${id}`)
+        .then(res => {
+            console.log(res);
+        })
+        setApplications(applications.filter(applications => applications.id !== id));
     }
 
     const renderTable = () => {
@@ -31,18 +53,38 @@ const DataTable = () => {
                     application => 
                     <tr key={application.id}>
                         <td>
-                            <input type="checkbox" />
+                            <input type="checkbox" onChange={() => addChecked(application.id)} />
+                        </td>
+                        <td>
+                            {application.appdate != null ? moment(application.appdate).format("MM/DD/YYYY") : ""}
                         </td>
                         <td>
                             {application.title}
                         </td>
                         <td>
-                            <a href={application.url}>
+                            <a href={application.url.startsWith('https://') ||  application.url.startsWith('http://') ? application.url : "https://" + application.url}>
                                 {application.url}
                             </a>
                         </td>
                         <td>
-                            {application.status}
+                            {(() => {
+                                switch(application.status) {
+                                    case 'Applied':
+                                        return <Alert variant="primary">{application.status}</Alert>;
+                                    case 'Interviewing':
+                                        return <Alert variant="secondary">{application.status}</Alert>;
+                                    case 'Offer':
+                                        return <Alert variant="success">{application.status}</Alert>;
+                                    case 'Rejected':
+                                        return <Alert variant="danger">{application.status}</Alert>;
+                                    case 'No Response':
+                                        return <Alert variant="light">{application.status}</Alert>;
+                                    case 'Offer Accepted':
+                                        return <Alert variant="info">{application.status}</Alert>;
+                                    case 'Offer Rejected':
+                                        return <Alert variant="light">{application.status}</Alert>;
+                                }
+                            })()}
                         </td>
                     </tr>
                 )
@@ -50,25 +92,34 @@ const DataTable = () => {
     }
 
     return (
-        <Table hover>
-            <thead>
-                <tr>
-                    <th></th>
-                    <th>
-                        Title
-                    </th>
-                    <th>
-                        Url
-                    </th>
-                    <th>
-                        Status
-                    </th>
-                </tr>
-            </thead>
-            <tbody>
-                {renderTable()}
-            </tbody>
-        </Table>
+        <div style={{background: "white", padding: "50px"}}>
+            <h5 style={{paddingBottom: "20px"}}>Job Applications</h5>
+            <div>{selectedIds != "" ? editBox() : ""}</div>
+            <Table hover style={{border: "1px solid #ddd"}}>
+                <thead>
+                    <tr>
+                        <th>
+                            <input type="checkbox" />
+                        </th>
+                        <th>
+                            Date
+                        </th>
+                        <th>
+                            Title
+                        </th>
+                        <th>
+                            Url
+                        </th>
+                        <th style={{textAlign: "center"}}>
+                            Status
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {renderTable()}
+                </tbody>
+            </Table>
+        </div>
     );
 }
  
